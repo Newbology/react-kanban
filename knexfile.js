53daa74aecf8d19af102a33ea403ecb655795d1d
kevin tauzer <kevint@kevins-MBP.home>
2019-02-11 18:58:21 -1000

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      user: 'newbology',
+      password: 'password',
+      database: 'express_gallery'
+    },
+    migrations: {
+      directory: __dirname + '/knex/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/knex/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: 'postgres-primary-db',
+      user: 'newbology',
+      password: 'password',
+      database: 'express_gallery'
+    },
+    migrations: {
+      directory: __dirname + '/knex/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/knex/seeds'
+    }
+  }
+};
