4b6e2730e6731bf37b033987f45f3e9bd6f3d809
kevin tauzer <kevint@kevins-MBP.home>
2019-02-14 12:15:34 -1000

    will revise in another pull request

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,9 @@
 import { ADD_TASK, DELETE_TASK, EDIT_TASK, LOAD_TASK } from '../actions';
 
-let taskId = 0
-
-const tasks = [
-{
-  id:1,
-  title: "Feed Dogs",
-  createdBy: "Dad",
-  assignedTo:"Kid",
-  Priority: "High"
-}
-];
 
 const taskReducer = (state = tasks, action) => {
   switch (action.type) {
     case ADD_TASK:
-      action.payload.id = taskId++;
-      return [...state, action.payload];
     case DELETE_TASK: return;
     case EDIT_TASK: return;
     case LOAD_TASK: return;
