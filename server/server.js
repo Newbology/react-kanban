a3d747968d3595781a3cdd93d19abf5d8101ddea
kevin tauzer <kevint@kevins-MBP.home>
2019-02-14 12:13:14 -1000

    remove user auth, change redis session to be more secure

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ const kanban = require('./routes/kanban')
 const PORT = process.env.PORT || 8080;
 const ENV = process.env.NODE_ENV || 'development';
 const SESSION_SECRET = process.env.SESSION_SECRET || 'keyboard cat';
-const saltRounds = 12;
-
+const REDIS = process.env.REDIS_HOST_PORT;
+const REDIS_URL = process.env.REDIS_URL;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,7 +21,7 @@ app.use(express.static('public'));
 app.use(flash());
 
 app.use(session({
-  store: new redis({ url: 'redis://redis-server:6379', logErrors:true}),
+  store: new redis({ url: `${REDIS_URL}:${REDIS}`, logErrors:true}),
   secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
@@ -30,117 +30,6 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-passport.serializeUser((user, done) => {
-  console.log('serializing');
-  return done(null, {
-    id: user.id,
-    username: user.username
-  });
-});
-
-passport.deserializeUser((user, done) => {
-  console.log('deserializing');
-  new User({ id: user.id }).fetch()
-  .then(user => {
-    user = user.toJSON();
-    return done(null, {
-      id: user.id,
-      username: user.username
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-    return done(err);
-  });
-});
-
-
-passport.use(new LocalStrategy(function(username, password, done) {
-  return new User({ username: username })
-  .fetch()
-  .then ( user => {
-    user = user.toJSON();
-    console.log(user);
-    
-    if (user === null) {
-      return done(null, false, {message: 'bad username or password'});
-    }
-    else {
-      bcrypt.compare(password, user.password)
-      .then((res) => {
-        if (res) { return done(null, user); }
-        else {
-          return done(null, false, {message: 'bad username or password'});
-        }
-      });
-    }
-  })
-  .catch(err => {
-    console.log('error: ', err);
-    return done(err);
-  });
-}));
-app.post('/register', (req, res) => {
-  bcrypt.genSalt(saltRounds,(err, salt) => {
-    if (err) { console.log(err); }
-    
-    bcrypt.hash(req.body.password, salt, function(err, hash){
-      if (err) { console.log(err); }
-
-      return new User({
-        username: req.body.username,
-        password: hash
-      })
-      .save()
-      .then((user) => {
-        console.log(user);
-        req.flash('authErr', 'You have successfully created an account!')
-        res.redirect('/login');
-      })
-      .catch((err) => {
-        console.log(err);
-        return res.send('Error Creating account');
-      });
-    });
-  });
-});
-
-app.get('/login', (req, res) => {
-  res.render('gallery/login', {message: req.flash('authErr')});
-});
-
-app.get('/register', (req, res) => {
-  res.render('gallery/register')
-});
-
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/secret',
-  failureRedirect: '/login',
-  failureFlash: true
-}));
-
-app.get('/logout', (req, res) => {
-  req.logout();
-  res.sendStatus(200);
-});
-
-
-function isAuthenticated (req, res, next) {
-  if(req.isAuthenticated()) { next();}
-  else {req.flash('authErr', 'Must be logged in to do that'), res.redirect('/login'); }
-}
-
-app.get('/secret', isAuthenticated, (req, res) => {
-  console.log('req.user: ', req.user);
-  console.log('req.user id', req.user.id);
-  console.log('req.username', req.user.username);
-  res.redirect('gallery');
-});
-
-app.get('/smoke', (req, res) => {
-  return res.send('smoke test');
-});
 
 
 app.use('/api', kanban)
