81210e9059117c02e70dca477edc5f208da58619
kevin tauzer <kevint@kevins-MBP.home>
2019-02-14 16:14:24 -1000

    change PG to POSTGRES to connect database

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@ require('dotenv').config({ path: './.env'});
 module.exports = {
     client: 'pg',
     connection: {
-      host: process.env.PG_HOST,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE
     },
     pool: {
       min: 2,
