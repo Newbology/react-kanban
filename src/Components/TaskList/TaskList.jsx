07c49e9996e53d832e642c4482075c9293da61c3
kevin tauzer <kevint@kevins-MBP.home>
2019-02-14 18:23:07 -1000

    add react-connect

--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -1,14 +1,18 @@
 import React, { Component }from 'react';
+import { connect } from 'react-redux';
 import Task from '../Task';
 
 
 class TaskList extends Component {
   constructor(props){
+ 
     super(props);
   }
   render(){
   return(
+  
     <div className="Task">
+    
       <Task 
       title={task.title}
       body={task.body}
@@ -19,6 +23,20 @@ class TaskList extends Component {
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return{}
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return{}
+}
+
+TaskList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TaskList)
+
 
 
 export default TaskList;
