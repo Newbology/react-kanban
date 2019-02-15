784507f542fe246d01c337afa181929ccd0cc039
kevin tauzer <kevint@kevins-MBP.home>
2019-02-14 18:22:15 -1000

    got rid of dummy data in server

--- server/server.js
+++ server/server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const bcrypt = require('bcryptjs');
 const redis = require('connect-redis')(session);
 const flash = require('connect-flash');
 const kanban = require('./routes/kanban')
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.EXPRESS_CONTAINER_PORT;
 const ENV = process.env.NODE_ENV || 'development';
 const SESSION_SECRET = process.env.SESSION_SECRET || 'keyboard cat';
 const REDIS = process.env.REDIS_HOST_PORT;
@@ -31,36 +29,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-const tasks = [
-  {
-    id: 1,
-    title: "todo",
-    body: 'Dr. NoClue more tex tto have a body not right spelling/grammar',
-    createdBy:'jim',
-    assignedTo:'joe',
-    priority:'medium'
 
-  },
-  {
-    id: 2,
-    title: "sit down",
-    body: 'Dr. NoClue lots of text to have a body',
-    createdBy:'jim',
-    assignedTo:'joe',
-    priority:'high'
-
-  },
-  {
-    id: 3,
-    title: "go town",
-    body: 'Dr. NoClu mexico city utah e',
-    createdBy:'jim',
-    assignedTo:'joe',
-    priority:'low'
-
-  },
-
-];
 
 app.use('/api', kanban)
 
