634da60f59ef54053cc4742714c39dba4a8e02b7
kevin tauzer <kevint@kevins-MBP.home>
2019-02-15 16:49:45 -1000

    let data flow to kanbanboard

--- src/containers/KanbanBoard/KanbanBoard.jsx
+++ src/containers/KanbanBoard/KanbanBoard.jsx
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
 import Column from '../../Components/Column';
-import TaskList from '../../Components/TaskList';
+import { connect } from 'react-redux';
 
 
 
 class KanbanBoard extends Component {
-  constructor(props){
-    super(props)
-
-    this.state = {
-      input: ''
-    };
-  };
 
   render() {
+    let inQueue = [];
+    let inProg = [];
+    let completed = [];
+    
+    this.props.tasks.forEach((tasks) => {
+      switch(tasks.status_id){
+      case 1:
+        inQueue.push(tasks)
+        break;
+      case 2: 
+        inProg.push(tasks)
+        break;
+      case 3:
+        completed.push(tasks)
+        break;
+      default:
+        break
+      };
+    });
+
     return(
     <div className="kanban">
-      <Column column={this.props.column}/>
-      <TaskList tasks={this.state.props}/>
+      <Column title="inQueue" column={inQueue} />
+      <Column title="inProgress" column={inProg}/>
+      <Column title="Completed" column={completed}/>
     </div>
     )
   };
 };
 
+
+
+const mapStateToProps = (state) => {
+  return{
+    tasks: state
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return{};
+};
+
+KanbanBoard = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(KanbanBoard);
 
 export default KanbanBoard;
