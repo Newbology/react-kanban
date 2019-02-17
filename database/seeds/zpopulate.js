fa38a716fc4af510233a381976282ae56c932524
kevin tauzer <kevint@kevins-MBP.home>
2019-02-16 14:37:25 -1000

    remove name on id

--- database/seeds/zpopulate.js
+++ database/seeds/zpopulate.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('tasks').insert([
         {
-         task_id: 1,
+         id: 1,
          title: 'GitGud',
          body:'practice git',
          priority_id:1,
@@ -15,7 +15,7 @@ exports.seed = function(knex, Promise) {
          assignedTo_id:1
         },
         {
-         task_id: 2,
+         id: 2,
          title: 'Prayer',
          body:'raise prayer to 99',
          priority_id:2,
@@ -24,7 +24,7 @@ exports.seed = function(knex, Promise) {
          assignedTo_id:3
         },
         {
-         task_id: 3,
+         id: 3,
          title: 'Strength',
          body:'Raise prayer to 99',
          priority_id:1,
@@ -33,7 +33,7 @@ exports.seed = function(knex, Promise) {
          assignedTo_id:2
         },
         {
-         task_id: 4,
+         id: 4,
          title: 'Runecrafting',
          body:'Raise Runecrafting to 99',
          priority_id:1,
