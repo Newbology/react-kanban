a84d8fcaed8b104e92b31016dddd18257cb7dcbc
kevin tauzer <kevint@kevins-MBP.home>
2019-02-16 14:42:26 -1000

    add css on tasklist

--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Task from '../Task';
-
+import './TaskList.scss';
 
 const TaskList = (props) => {
   const list = props.tasks.map((task) => {
 
     return(
-      <Task 
-      key={task.task_id}
-      id={task.task_id}
+      <Task
+      key={task.id}
+      id={task.id}
       title={task.title}
       body={task.body}
       priority={task.priority_id}
@@ -18,10 +18,11 @@ const TaskList = (props) => {
       />
     );
   });
+  
   return(
-  <>
+  <div className="list-wrapper">
   {list}
-  </>
+  </div>
   );
 };
 
