c4d136ac5cef3885522fb38f5d543b6ae51b241f
kevin tauzer <kevint@kevins-MBP.home>
2019-02-16 14:43:47 -1000

    async to add task

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,24 +4,27 @@ export const EDIT_TASK = 'EDIT_TASK';
 export const LOAD_TASK = 'LOAD_TASK';
 
 export function addTask(newTask){
-  return{
-    type: ADD_TASK,
-    payload: newTask
+  return (dispatch) => {
+    return fetch('/kanban', {
+      method: 'POST',
+      body: JSON.stringify(newTask),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((newTask) => {
+      return dispatch({
+        type: ADD_TASK,
+        payload: newTask
+      });
+    });
   };
 };
 
-export function addAsyncTask(newTask){
-  return dispatch => {
-    
-  };
-};
 
-export function deleteTask(Task){
-  return{
-    type: DELETE_TASK,
-    payload: Task
-  };
-};
 
 export const loadTasks = () => {
   return (dispatch) => {
